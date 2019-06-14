5da157f3f37e0192223be4d834533e715e6de881
ngoc426123 <48208982+ngoc426123@users.noreply.github.com>
2019-06-14 18:33:01 +0700

    18h32 14/06/2019

--- roll/js/main/main.js
+++ roll/js/main/main.js
@@ -6,9 +6,8 @@ $(document).ready(function(){
         dots:true,
     });
     $("#slideService").slick({
-    	slidesToShow:4,
+    	slidesToShow:3,
     	autoplay:true,
-        dots:true,
         responsive: [
             {
                 breakpoint: 991,
@@ -30,45 +29,8 @@ $(document).ready(function(){
             }
         ]
     });
-    $('#feeling_img').slick({
-        asNavFor: '#feeling_text',
-        draggable: false,
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerPadding: '0px',
-        adaptiveHeight: false,
-        focusOnSelect: true,
-        speed: 500,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 520,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-    $('#feeling_text').slick({
-        asNavFor: '#feeling_img',
-        draggable: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        swipe: false,
-        adaptiveHeight: false,
-        focusOnSelect: true,
-        arrows: false,
-        autoplay: true,
-        speed: 500
-    });
     $("#slidePartner").slick({
-        slidesToShow:5,
+        slidesToShow:6,
         autoplay:true,
         responsive: [
             {
