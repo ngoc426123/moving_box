0950433394c31a787eccc131e182ec5b6fb19182
ngoc426123 <48208982+ngoc426123@users.noreply.github.com>
2019-07-27 22:19:49 +0700

    22h19 27/07/2019
    Start email template

--- roll/wp-content/themes/moving_box_theme/content/content-boxregis.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$link = get_page_link('popup-tu-van');
-?>
-<div class="boxRegis">
-	<div class="t1">Bạn muốn chúng tôi tư vấn giúp bạn</div>
-	<div class="linkPopup"><a href="<?php echo __($link); ?>"><span>Click vào để gởi câu hỏi</span></a></div>
-	<div class="t2">hoặc gọi cho chúng tôi qua số <span>0909 919293</span></div>
-</div>
